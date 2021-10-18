ae9a64c33a81f07670023aaf034a1f60e78de6f5
Tuba Özyurt <tuba.ozyurt93@gmail.com>
2021-10-19 00:03:00 +0300

    18 Ekim ders sonu

---
+++ src/app.js
@@ -0,0 +1,14 @@
+console.log("Merhaba Kodlama.io");
+
+var dolarDun=9.20;
+
+console.log(dolarDun);
+
+let konutKredileri= ["Emlak konut","Kamu konut"]
+console.log(konutKredileri);
+
+console.log("<ul>")
+for(let i=0;i<3;i++) {
+    console.log("<li>"+i+"</li>")
+}
+console.log("</ul>")
